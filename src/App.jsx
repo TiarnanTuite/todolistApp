8036f8711ea523710c17176ec124ad5e214dbfb3
Your UsernameTiarnanTuite <tuitetiarnan@gmail.com>
2025-01-09 15:44:18 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
         <div className='todo-input'>
           <div className='todo-input-item'>
             <label>Title</label>
-            <input type="text" placeholder="What's the tast title?"/>
+            <input type="text" placeholder="What's the task title?"/>
           </div>
           <div className='todo-input-item'>
             <label>Description</label>
-            <input type="text" placeholder="What's the tast description?"/>
+            <input type="text" placeholder="What's the task description?"/>
           </div>
           <div className='todo-input-item'>
             <button type='button' className='primaryBtn'>Add</button>
