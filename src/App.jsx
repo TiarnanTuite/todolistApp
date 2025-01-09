86202ab580183c984d22afa27e5110b6ca313ed6
Your UsernameTiarnanTuite <tuitetiarnan@gmail.com>
2025-01-09 16:01:31 +0000

    created the basic input fields and buttons that will be used for my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+//main function
 function App() {
   return (
     <div className='App'>
+      {/*Heading */}
       <h1>My Todos</h1>
 
+      {/*making the input boxes */}
       <div className='todo-wrapper'>
         <div className='todo-input'>
           <div className='todo-input-item'>
@@ -19,6 +22,20 @@ function App() {
           <div className='todo-input-item'>
             <button type='button' className='primaryBtn'>Add</button>
           </div>
+        </div>
+
+          {/*Buttons to enter text */}
+        <div className='bnt-area'>
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+        <div className='todo-list'>
+
+          <div className='todo-list-item'>
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+
         </div>
       </div>
     </div>
